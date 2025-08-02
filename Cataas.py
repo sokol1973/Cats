4a802e0e5910a890107ce5e04e080db8b5604015
Proba <brYuliya73@yandex.ru>
2025-08-02 11:37:09 +0300

    Добавлен комбобокс для выбора тегов, а поле энтри удалено

--- Cataas.py
+++ Cataas.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+Allowed_tags = ["sleep","jump","fight","black","white","siamese","cute",]
 
 def load_image(url):
     try:
@@ -18,7 +20,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -40,14 +42,6 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text = "Загрузить по тегу", command = open_new_window)
-load_button.pack()
-
-
-
 # update_button = Button(text = "Обновить", command= set_image)
 # update_button.pack()
 
@@ -61,6 +55,15 @@ file_menu.add_separator()
 file_menu.add_command(label = "Выход", command = exit)
 
 url = "https://cataas.com/cat"
+
+tag_label = Label(text = "выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values = Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text = "Загрузить по тегу", command = open_new_window)
+load_button.pack()
 
 
 window.mainloop()
