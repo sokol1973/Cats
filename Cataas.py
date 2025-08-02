fbdb1875815b38991bd66e7f094b8c49f6f505c4
Proba <brYuliya73@yandex.ru>
2025-08-02 11:21:17 +0300

    Добавлено получение изображений по тего, а также без тега

--- Cataas.py
+++ Cataas.py
@@ -18,7 +18,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
 
     if img:
        new_window = Toplevel()
@@ -37,6 +39,12 @@ def exit():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
+
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text = "Загрузить по тегу", command = open_new_window)
+load_button.pack()
 
 
 
